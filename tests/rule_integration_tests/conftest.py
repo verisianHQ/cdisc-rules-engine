67154486cbe4214d5ce05bcd33ccc6bab0a13d74
PHJLK <henning@verisian.com>
2025-07-22 19:34:27 +0100

    - cleaning tests

--- tests/rule_integration_tests/conftest.py
+++ tests/rule_integration_tests/conftest.py
@@ -10,3 +10,58 @@ def get_core_rule(pytestconfig) -> dict:
         return rules.get(rule)
 
     return _call_core_rule
+
+
+@pytest.fixture
+def get_sample_lb_rule() -> dict:
+    return {
+        "core_id": "QC.CDISC.SDTMIG.CG0032",
+        "classes": {"Include": ["ALL"]},
+        "domains": {"Include": ["ALL"]},
+        "rule_type": "Range & Limit",
+        "sensitivity": "Record",
+        "severity": "error",
+        "Authorities": [{"Standards": [{"Name": "SDTMIG", "Version": "3.4"}]}],
+        "standards": [{"Name": "SDTMIG", "Version": "3.4"}],
+        "conditions": {
+            "all": [
+                {
+                    "name": "get_dataset",
+                    "operator": "greater_than",
+                    "value": {"target": "LBSEQ", "comparator": 0},
+                }
+            ]
+        },
+        "actions": [
+            {
+                "name": "generate_dataset_error_objects",
+                "params": {"message": "LBSEQ greater than 0"},
+            }
+        ],
+    }
+
+
+@pytest.fixture
+def get_sample_lb_dataset() -> dict:
+    return {
+        "filename": "lb.xpt",
+        "label": "Laboratory Test Results",
+        "variables": [
+            {
+                "name": "DOMAIN",
+                "label": "Domain Abbreviation",
+                "type": "Char",
+                "length": 4,
+            },
+            {
+                "name": "LBSEQ",
+                "label": "Sequence Number",
+                "type": "Num",
+                "length": 8,
+            },
+        ],
+        "records": {
+            "DOMAIN": ["LB", "LB"],
+            "LBSEQ": [1, 2],
+        },
+    }
--- tests/rule_integration_tests/test_rule_types.py
+++ tests/rule_integration_tests/test_rule_types.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from scripts.run_validation import run_single_rule_validation
+from scripts.run_sql_validation import sql_run_single_rule_validation
 
 
 def test_test(get_core_rule):
@@ -11,58 +11,11 @@ def test_test(get_core_rule):
 
 
 @patch("cdisc_rules_engine.services.data_services.DummyDataService.get_dataset_class")
-def test_rule_existing_rule(mock_get_dataset_class):
-    datasets = [
-        {
-            "filename": "lb.xpt",
-            "label": "Laboratory Test Results",
-            "variables": [
-                {
-                    "name": "DOMAIN",
-                    "label": "Domain Abbreviation",
-                    "type": "Char",
-                    "length": 4,
-                },
-                {
-                    "name": "LBSEQ",
-                    "label": "Sequence Number",
-                    "type": "Num",
-                    "length": 8,
-                },
-            ],
-            "records": {
-                "DOMAIN": ["LB", "LB"],
-                "LBSEQ": [1, 2],
-            },
-        }
-    ]
-    rule = {
-        "core_id": "QC.CDISC.SDTMIG.CG0032",
-        "classes": {"Include": ["ALL"]},
-        "domains": {"Include": ["ALL"]},
-        "rule_type": "Range & Limit",
-        "sensitivity": "Record",
-        "severity": "error",
-        "Authorities": [{"Standards": [{"Name": "SDTMIG", "Version": "3.4"}]}],
-        "standards": [{"Name": "SDTMIG", "Version": "3.4"}],
-        "conditions": {
-            "all": [
-                {
-                    "name": "get_dataset",
-                    "operator": "greater_than",
-                    "value": {"target": "LBSEQ", "comparator": 0},
-                }
-            ]
-        },
-        "actions": [
-            {
-                "name": "generate_dataset_error_objects",
-                "params": {"message": "LBSEQ greater than 0"},
-            }
-        ],
-    }
+def test_rule_existing_rule(
+    mock_get_dataset_class, get_sample_lb_rule, get_sample_lb_dataset
+):
     mock_get_dataset_class.return_value = None
-    data = run_single_rule_validation(datasets, rule)
+    data = sql_run_single_rule_validation([get_sample_lb_dataset], get_sample_lb_rule)
     assert "LB" in data
     assert len(data["LB"]) == 1
     assert data["LB"][0]["message"] == "LBSEQ greater than 0"
