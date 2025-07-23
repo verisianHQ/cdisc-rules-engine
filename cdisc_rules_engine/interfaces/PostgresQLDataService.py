4bd19f6d24e3df3349301f150cb5b23ee5061d5f
PHJLK <henning@verisian.com>
2025-07-23 14:04:51 +0100

    - added SQLDataservice into the run_sql function, moved over the content data and content metadata part of the input data from tests

--- cdisc_rules_engine/interfaces/PostgresQLDataService.py
+++ cdisc_rules_engine/interfaces/PostgresQLDataService.py
@@ -1,11 +1,61 @@
+import pandas as pd
+
 from pathlib import Path
 
-from cdisc_rules_engine.interfaces import SQLDataService
+from cdisc_rules_engine.interfaces.SQLDataService import SQLDataService
+from cdisc_rules_engine.models.TestDataset import TestDataset
 
 
 class PostgresQLDataService(SQLDataService):
 
-    def from_list_of_dict(
+    def __init__(
+        self,
+        datasets_path: Path = None,
+        define_xml_path: Path = None,
+        terminology_paths: dict = None,
+        content_dfs: list[pd.DataFrame] = None,
+        metadata_dfs: list[pd.DataFrame] = None,
+    ):
+        super().__init__(datasets_path, define_xml_path, terminology_paths)
+        self.content_dfs = content_dfs
+        self.metadata_dfs = metadata_dfs
+
+    @classmethod
+    def from_list_of_testdatasets(cls, test_datasets: list[TestDataset]) -> None:
+        """
+        Constructor for tests, passing in TestDataset
+        and create corresponding SQL tables, setting path to "memory"
+        """
+        content_dfs = []
+        metadata_dfs = []
+        for test_dataset in test_datasets:
+            # Collect content
+            cdf = pd.DataFrame.from_records(test_dataset["records"])
+            cdf.columns = [col.lower() for col in cdf.columns]
+            content_dfs.append(cdf)
+
+            # Collect variable metadata
+            mdf = pd.DataFrame()
+            for test_variable in test_dataset["variables"]:
+                new_row = pd.DataFrame(
+                    {
+                        "domain": [test_dataset["filename"].split(".")[0].upper()],
+                        "name": [test_variable["name"]],
+                        "label": [test_variable["label"]],
+                        "type": [test_variable["type"]],
+                        "length": [test_variable["length"]],
+                    }
+                )
+                mdf = pd.concat([mdf, new_row], ignore_index=True)
+            metadata_dfs.append(mdf)
+
+        print(content_dfs)
+        print(metadata_dfs)
+
+        return cls(None, None, None, content_dfs, metadata_dfs)
+
+    @classmethod
+    def from_list_of_dicts(
         cls, data_dicts: dict[str, list[dict]], define_xml_path: Path, terminology_paths: dict
     ) -> None:
         """
@@ -15,6 +65,7 @@ class PostgresQLDataService(SQLDataService):
         """
         pass
 
+    @classmethod
     def from_list_of_records(
         cls, records: dict[str, dict[str, list[any]]], define_xml_path: Path, terminology_paths: dict
     ) -> None:
