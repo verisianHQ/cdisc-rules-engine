cf2943414d393d6dda46cc58663a779827ee456e
PHJLK <henning@verisian.com>
2025-07-23 09:28:49 +0100

    - re-routing test into SQL branch of files

--- tests/rule_integration_tests/test_rule_types.py
+++ tests/rule_integration_tests/test_rule_types.py
@@ -13,7 +13,9 @@ def test_test(get_core_rule):
 @patch("cdisc_rules_engine.services.data_services.DummyDataService.get_dataset_class")
 def test_rule_existing_rule(mock_get_dataset_class, get_sample_lb_rule, get_sample_lb_dataset):
     mock_get_dataset_class.return_value = None
-    data = sql_run_single_rule_validation([get_sample_lb_dataset], get_sample_lb_rule)
+    data = sql_run_single_rule_validation(
+        [get_sample_lb_dataset], get_sample_lb_rule, None, None, "SDTMIG", "3.4", None, []
+    )
     assert "LB" in data
     assert len(data["LB"]) == 1
     assert data["LB"][0]["message"] == "LBSEQ greater than 0"
