e64ac068609fb33cfda86990a2429fb9bcb4654e
PHJLK <henning@verisian.com>
2025-07-28 16:54:50 +0100

    - revert dataset builder, bc will be completely abandoned

--- cdisc_rules_engine/sql_rules_engine.py
+++ cdisc_rules_engine/sql_rules_engine.py
@@ -183,7 +183,6 @@ class SQLRulesEngine:
         rule: dict,
         datasets: Iterable[SDTMDatasetMetadata],
         dataset_metadata: SDTMDatasetMetadata,
-        sql_dataset_metadata: SQLDatasetMetadata,
     ):
         return builder_factory.get_service(
             rule.get("rule_type"),
@@ -194,7 +193,7 @@ class SQLRulesEngine:
             rule_processor=self.rule_processor,
             dataset_metadata=dataset_metadata,
             datasets=datasets,
-            dataset_path=sql_dataset_metadata.filepath,
+            dataset_path=dataset_metadata.filepath,
             define_xml_path=self.define_xml_path,
             standard=self.standard,
             standard_version=self.standard_version,
@@ -215,7 +214,7 @@ class SQLRulesEngine:
         It defines a rule validator based on its type and calls it.
         """
         kwargs = {}
-        builder = self.get_dataset_builder(rule, datasets, dataset_metadata, sql_dataset_metadata)
+        builder = self.get_dataset_builder(rule, datasets, dataset_metadata)
         dataset = builder.get_dataset()
         # Update rule for certain rule types
         # SPECIAL CASES FOR RULE TYPES ###############################
