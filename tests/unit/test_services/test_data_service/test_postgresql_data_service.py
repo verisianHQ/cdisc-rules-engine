27ec56166139a4f7410050178aba0c4719330033
PHJLK <henning@verisian.com>
2025-07-29 16:46:55 +0100

    - make sense but tests do not pass

--- tests/unit/test_services/test_data_service/test_postgresql_data_service.py
+++ tests/unit/test_services/test_data_service/test_postgresql_data_service.py
@@ -2,18 +2,13 @@ from cdisc_rules_engine.data_service.postgresql_data_service import PostgresQLDa
 
 
 def test_get_dataset_metadata_sql(get_sample_lb_dataset, get_sample_supp_dataset):
-    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_lb_dataset])
-    ds_metadata = sql_data_service.get_dataset_metadata("LB")
+    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_lb_dataset, get_sample_supp_dataset])
+    ds_metadata = sql_data_service.get_dataset_metadata(dataset_id="LB")
     assert 2 == len(ds_metadata.variables)
-
-    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_supp_dataset])
-    ds_metadata = sql_data_service.get_dataset_metadata("SUPPDM")
+    ds_metadata = sql_data_service.get_dataset_metadata(dataset_id="SUPPDM")
     assert 3 == len(ds_metadata.variables)
 
 
-def test_get_rdomain(get_sample_lb_dataset, get_sample_supp_dataset):
-    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_lb_dataset])
-    assert sql_data_service.get_rdomain("LB") is None
-
-    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_supp_dataset])
-    assert "DM" == sql_data_service.get_rdomain("SUPPDM")
+def test_get_uploaded_dataset_ids(get_sample_lb_dataset, get_sample_supp_dataset):
+    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([get_sample_lb_dataset, get_sample_supp_dataset])
+    assert 2 == len(sql_data_service.get_uploaded_dataset_ids())
