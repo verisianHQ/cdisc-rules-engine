07991d2b790f9a2ac0a2c66245c312f548645376
davidverisian <david@verisian.com>
2025-08-29 13:01:33 +0300

    add case insensitive operators

--- cdisc_rules_engine/check_operators/sql_operators.py
+++ cdisc_rules_engine/check_operators/sql_operators.py
@@ -633,24 +633,19 @@ class PostgresQLOperators(BaseType):
     @log_operator_execution
     @type_operator(FIELD_DATAFRAME)
     def is_contained_by_case_insensitive(self, other_value):
-        """target = self.replace_prefix(other_value.get("target"))
-        comparator = other_value.get("comparator", [])
-        value_is_literal = other_value.get("value_is_literal", False)
+        comparator = other_value["comparator"]
         if isinstance(comparator, list):
-            comparator = [val.lower() for val in comparator]
-        elif isinstance(comparator, str) and not value_is_literal:
-            # column name provided
-            comparator = self.replace_prefix(comparator)
-        comparison_data = self.get_comparator_data(comparator, value_is_literal)
-        if self.is_column_of_iterables(comparison_data):
-            results = vectorized_case_insensitive_is_in(self.validation_df[target].str.lower(), comparison_data)
-            return self.validation_df.convert_to_series(results)
-        elif self.validation_df.is_series(comparison_data):
-            results = self.validation_df[target].str.lower().isin(comparison_data.str.lower())
-        else:
-            results = self.validation_df[target].str.lower().isin(comparison_data)
-        return results"""
-        raise NotImplementedError("is_contained_by_case_insensitive check_operator not implemented")
+            comparator = [str(v).lower() for v in comparator]
+        elif isinstance(comparator, str):
+            comparator = comparator.lower()
+
+        return self.is_contained_by(
+            {
+                "target": f"LOWER({self.replace_prefix(other_value['target']).lower()})",
+                "comparator": comparator,
+                "value_is_literal": other_value.get("value_is_literal", False),
+            }
+        )
 
     @log_operator_execution
     @type_operator(FIELD_DATAFRAME)
--- tests/unit/test_check_operators/test_sql_containment_checks.py
+++ tests/unit/test_check_operators/test_sql_containment_checks.py
@@ -8,26 +8,30 @@ from cdisc_rules_engine.data_service.postgresql_data_service import (
 
 
 @pytest.mark.parametrize(
-    "data,comparator,expected_result",
+    "data,comparator,value_is_literal,expected_result",
     [
         (
             {"target": ["Ctt", "Btt", "A"]},
             ["Ctt", "B", "A"],
+            True,
             [True, False, True],
         ),
         (
             {"target": ["A", "B", "C"]},
             ["C", "Z", "A"],
+            True,
             [True, False, True],
         ),
         (
             {"target": ["A", "B", "C"], "VAR2": ["A", "B", "D"]},
             "VAR2",
+            False,
             [True, True, False],
         ),
         (
             {"target": ["A", "B", "C"]},
             "B",
+            True,
             [False, True, False],
         ),
         # Note: Doesn't seem like there is a way to test this using SQL
@@ -38,13 +42,10 @@ from cdisc_rules_engine.data_service.postgresql_data_service import (
         # ),
     ],
 )
-def test_is_contained_by(data, comparator, expected_result):
+def test_is_contained_by(data, comparator, value_is_literal, expected_result):
     table_name = "test_table"
     tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
     sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
-
-    # Determine if comparator is a literal list
-    value_is_literal = isinstance(comparator, list)
 
     result = sql_ops.is_contained_by(
         {"target": "target", "comparator": comparator, "value_is_literal": value_is_literal}
@@ -53,42 +54,108 @@ def test_is_contained_by(data, comparator, expected_result):
 
 
 @pytest.mark.parametrize(
-    "data,comparator,expected_result",
+    "data,comparator,value_is_literal,expected_result",
     [
-        # Test with list of literal values
         (
             {"target": ["Ctt", "Btt", "A"]},
             ["Ctt", "B", "A"],
+            True,
             [False, True, False],
         ),
         (
             {"target": ["A", "B", "C"]},
             ["C", "Z", "A"],
+            True,
             [False, True, False],
         ),
-        # Test with column reference
         (
             {"target": ["A", "B", "C"], "VAR2": ["A", "B", "D"]},
             "VAR2",
+            False,
             [False, False, True],
         ),
-        # Test with single literal value (3rd branch)
         (
             {"target": ["A", "B", "C"]},
             "B",
+            True,
             [True, False, True],
         ),
     ],
 )
-def test_is_not_contained_by(data, comparator, expected_result):
+def test_is_not_contained_by(data, comparator, value_is_literal, expected_result):
     table_name = "test_table"
     tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
     sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
 
-    # Determine if comparator is a literal list
-    value_is_literal = isinstance(comparator, list)
-
     result = sql_ops.is_not_contained_by(
+        {"target": "target", "comparator": comparator, "value_is_literal": value_is_literal}
+    )
+    assert result.equals(pd.Series(expected_result))
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    [
+        (
+            {"target": ["Ctt", "Btt", "A"]},
+            ["ctt", "b", "a"],
+            True,
+            [True, False, True],
+        ),
+        (
+            {"target": ["A", "B", "C"]},
+            ["c", "z", "a"],
+            True,
+            [True, False, True],
+        ),
+        (
+            {"target": ["A", "B", "C"]},
+            "b",
+            True,
+            [False, True, False],
+        ),
+    ],
+)
+def test_is_contained_by_case_insensitive(data, comparator, value_is_literal, expected_result):
+    table_name = "test_table"
+    tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
+    sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
+
+    result = sql_ops.is_contained_by_case_insensitive(
+        {"target": "target", "comparator": comparator, "value_is_literal": value_is_literal}
+    )
+    assert result.equals(pd.Series(expected_result))
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    [
+        (
+            {"target": ["Ctt", "Btt", "A"]},
+            ["ctt", "b", "a"],
+            True,
+            [False, True, False],
+        ),
+        (
+            {"target": ["A", "B", "C"]},
+            ["c", "z", "a"],
+            True,
+            [False, True, False],
+        ),
+        (
+            {"target": ["A", "B", "C"]},
+            "b",
+            True,
+            [True, False, True],
+        ),
+    ],
+)
+def test_is_not_contained_by_case_insensitive(data, comparator, value_is_literal, expected_result):
+    table_name = "test_table"
+    tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
+    sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
+
+    result = sql_ops.is_not_contained_by_case_insensitive(
         {"target": "target", "comparator": comparator, "value_is_literal": value_is_literal}
     )
     assert result.equals(pd.Series(expected_result))
