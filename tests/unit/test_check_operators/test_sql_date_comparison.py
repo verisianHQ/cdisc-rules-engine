1b8c1ff265acca4ceed3e81b3111750f6038a6c7
Summer Sim <summersim@Summers-MacBook-Pro.local>
2025-09-01 20:48:42 +0200

    moved unit tests to date_comparison_checks.py

--- tests/unit/test_check_operators/test_sql_date_comparison.py
+++ tests/unit/test_check_operators/test_sql_date_comparison.py
@@ -359,3 +359,57 @@ def test_sql_date_time_components(data, comparator, value_is_literal, date_compo
         }
     )
     assert result.equals(pd.Series(expected_result))
+
+
+@pytest.mark.parametrize(
+    "data,expected_complete",
+    [
+        (
+            {
+                "target": [
+                    "2020-01-01",
+                    "2020-01",
+                    "2020",
+                    "08/04",
+                    "1987-30",
+                    "2020-01-01T17:00:00+01:00",
+                    "2020-01-01T19:20+01:00",
+                ]
+            },
+            [True, False, False, False, False, True, True],
+        )
+    ],
+)
+def test_is_complete_date_sql(data, expected_complete):
+    table_name = "test_table"
+    tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
+    sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
+    result_complete = sql_ops.is_complete_date({"target": "target"})
+    assert result_complete.equals(pd.Series(expected_complete))
+
+
+@pytest.mark.parametrize(
+    "data,expected_incomplete",
+    [
+        (
+            {
+                "target": [
+                    "2020-01-01",
+                    "2020-01",
+                    "2020",
+                    "08/04",
+                    "1987-30",
+                    "2020-01-01T17:00:00+01:00",
+                    "2020-01-01T19:20+01:00",
+                ]
+            },
+            [False, True, True, True, True, False, False],
+        )
+    ],
+)
+def test_is_incomplete_date_sql(data, expected_incomplete):
+    table_name = "test_table"
+    tds = PostgresQLDataService.from_column_data(table_name=table_name, column_data=data)
+    sql_ops = PostgresQLOperators({"validation_dataset_id": table_name, "sql_data_service": tds})
+    result_incomplete = sql_ops.is_incomplete_date({"target": "target"})
+    assert result_incomplete.equals(pd.Series(expected_incomplete))
