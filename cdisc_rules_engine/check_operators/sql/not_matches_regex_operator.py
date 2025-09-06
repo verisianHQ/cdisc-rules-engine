e345c8e5d95ae6c7eb0fae122a10f4a3531d3e85
Samuel Johnson <sfjohnson24@gmail.com>
2025-09-06 13:18:48 -0400

    regex to main

--- cdisc_rules_engine/check_operators/sql/not_matches_regex_operator.py
+++ cdisc_rules_engine/check_operators/sql/not_matches_regex_operator.py
@@ -1,20 +1,13 @@
-from .base_sql_operator import BaseSqlOperator
+from .matches_regex_operator import MatchesRegexOperator
 
 
-class NotMatchesRegexOperator(BaseSqlOperator):
+class NotMatchesRegexOperator(MatchesRegexOperator):
     """Operator for inverted regex pattern matching."""
 
     def execute_operator(self, other_value):
-        target = self.replace_prefix(other_value.get("target")).lower()
-        target_column = self._column_sql(target)
+        """target = self.replace_prefix(other_value.get("target"))
         comparator = other_value.get("comparator")
-
-        def sql():
-            return f"""CASE WHEN
-                            {target_column} IS NOT NULL
-                            AND NOT ({target_column}::text ~ '{comparator}')
-                        THEN true
-                        ELSE false
-                        END"""
-
-        return self._do_check_operator(f"{target_column}_not_matches_regex", sql)
+        converted_strings = self.validation_df[target].map(lambda x: self._custom_str_conversion(x))
+        results = converted_strings.notna() & ~converted_strings.astype(str).str.match(comparator)
+        return results"""
+        raise NotImplementedError("not_matches_regex check_operator not implemented")
