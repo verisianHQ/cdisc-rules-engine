757ed04f8fd84fd7083251f732ea3cb5443d7ad4
Summer Sim <summersim@Summers-MacBook-Pro.local>
2025-09-09 15:02:31 +0100

    renaming methods according to contains_all operator for consistency

--- cdisc_rules_engine/check_operators/sql/contains_operator.py
+++ cdisc_rules_engine/check_operators/sql/contains_operator.py
@@ -17,33 +17,20 @@ class ContainsOperator(BaseSqlOperator):
         value_is_literal = other_value.get("value_is_literal", False)
         comparator = other_value.get("comparator")
 
-        cache_key = f"{target_column}_contains_{comparator}_{value_is_literal}_{self.case_insensitive}"
-
         if isinstance(comparator, str) and comparator in self.operation_variables:
-            sql = self._handle_operation_variable(target_column, comparator)
+            return self._handle_operation_variable_comparator(target_column, comparator)
         elif isinstance(comparator, list):
-            sql = self._handle_literal_list(target_column, comparator)
+            return self._handle_list_comparator(target_column, comparator)
         elif value_is_literal:
-            sql = self._handle_literal_value(target_column, comparator)
+            return self._handle_literal_value(target_column, comparator, value_is_literal)
         elif isinstance(comparator, str) and self._exists(comparator):
-            sql = self._handle_column_reference(target_column, comparator)
+            return self._handle_column_comparator(target_column, comparator)
         else:
-            sql = self._handle_literal_value(target_column, comparator)
+            return self._handle_invalid_comparator(target_column, comparator)
 
-        return self._do_check_operator(cache_key, sql)
-
-    def _handle_operation_variable(self, target_column, comparator):
-        """Handle operation variables (constants and collections)."""
-        variable = self.operation_variables[comparator]
-        if variable.type == "constant":
-            return self._create_constant_sql(target_column, comparator)
-        elif variable.type == "collection":
-            return self._create_collection_sql(target_column, comparator)
-        else:
-            raise ValueError(f"Unsupported operation variable type: {variable.type} " f"for variable {comparator}")
-
-    def _handle_literal_list(self, target_column, comparator):
-        """Handle literal lists (multiple values to check as substrings)."""
+    def _handle_list_comparator(self, target_column, comparator):
+        """Handle when comparator is a list."""
+        cache_key = f"{target_column}_contains_list_{self.case_insensitive}"
 
         def sql():
             target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
@@ -55,46 +42,25 @@ class ContainsOperator(BaseSqlOperator):
                           AND {target_sql} LIKE '%' || list_values.value || '%'
                       )"""
 
-        return sql
+        return self._do_check_operator(cache_key, sql)
 
-    def _handle_literal_value(self, target_column, comparator):
-        """Handle single literal value case."""
+    def _handle_operation_variable_comparator(self, target_column, comparator):
+        """Handle when comparator is an operation variable."""
+        variable = self.operation_variables[comparator]
+        cache_key = f"{target_column}_contains_opvar_{comparator}_{self.case_insensitive}"
 
-        def sql():
-            comparator_sql = self._constant_sql(comparator, lowercase=self.case_insensitive)
-            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
-            return f"""NOT ({self._is_empty_sql(target_column)})
-                      AND {comparator_sql} != ''
-                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
+        if variable.type == "constant":
+            return self._handle_constant_variable(target_column, comparator, cache_key)
+        elif variable.type == "collection":
+            return self._handle_collection_variable(target_column, comparator, cache_key)
+        else:
+            raise ValueError(
+                f"Unsupported operation variable type: {variable.type} "
+                f"for variable {comparator}. Expected 'collection' or 'constant'."
+            )
 
-        return sql
-
-    def _handle_column_reference(self, target_column, comparator):
-        """Handle column reference case."""
-
-        def sql():
-            comparator_sql = self._column_sql(comparator, lowercase=self.case_insensitive)
-            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
-            return f"""NOT ({self._is_empty_sql(target_column)})
-                      AND NOT ({self._is_empty_sql(comparator)})
-                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
-
-        return sql
-
-    def _create_constant_sql(self, target_column, comparator):
-        """Create SQL for constant operation variables."""
-
-        def sql():
-            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
-            comparator_sql = self._constant_sql(comparator, lowercase=self.case_insensitive)
-            return f"""NOT ({self._is_empty_sql(target_column)})
-                      AND {comparator_sql} != ''
-                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
-
-        return sql
-
-    def _create_collection_sql(self, target_column, comparator):
-        """Create SQL for collection operation variables."""
+    def _handle_collection_variable(self, target_column, comparator, cache_key):
+        """Handle collection type operation variable."""
 
         def sql():
             target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
@@ -106,4 +72,50 @@ class ContainsOperator(BaseSqlOperator):
                           AND {target_sql} LIKE '%' || collection_values.value || '%'
                       )"""
 
-        return sql
+        return self._do_check_operator(cache_key, sql)
+
+    def _handle_constant_variable(self, target_column, comparator, cache_key):
+        """Handle constant type operation variable."""
+
+        def sql():
+            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
+            comparator_sql = self._constant_sql(comparator, lowercase=self.case_insensitive)
+            return f"""NOT ({self._is_empty_sql(target_column)})
+                      AND {comparator_sql} != ''
+                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
+
+        return self._do_check_operator(cache_key, sql)
+
+    def _handle_column_comparator(self, target_column, comparator):
+        """Handle when comparator is a column name."""
+        comparator_column = self.replace_prefix(comparator)
+        cache_key = f"{target_column}_contains_{comparator_column}_{self.case_insensitive}"
+
+        def sql():
+            comparator_sql = self._column_sql(comparator, lowercase=self.case_insensitive)
+            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
+            return f"""NOT ({self._is_empty_sql(target_column)})
+                      AND NOT ({self._is_empty_sql(comparator)})
+                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
+
+        return self._do_check_operator(cache_key, sql)
+
+    def _handle_literal_value(self, target_column, comparator, value_is_literal):
+        """Handle single literal value case."""
+        cache_key = f"{target_column}_contains_literal_{comparator}_{value_is_literal}_{self.case_insensitive}"
+
+        def sql():
+            comparator_sql = self._constant_sql(comparator, lowercase=self.case_insensitive)
+            target_sql = self._column_sql(target_column, lowercase=self.case_insensitive)
+            return f"""NOT ({self._is_empty_sql(target_column)})
+                      AND {comparator_sql} != ''
+                      AND {target_sql} LIKE '%' || {comparator_sql} || '%'"""
+
+        return self._do_check_operator(cache_key, sql)
+
+    def _handle_invalid_comparator(self, target_column, comparator):
+        """Handle invalid comparator types."""
+        raise ValueError(
+            f"Invalid comparator type for contains operation on column '{target_column}'. "
+            f"Expected list, column name, or operation variable, but got: {type(comparator).__name__}"
+        )
