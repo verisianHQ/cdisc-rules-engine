83ed112ed7733c95bee5563c7245d7859303dac8
Summer Sim <summersim@Summers-MacBook-Pro.local>
2025-09-10 14:35:10 +0100

    compiled multiple operators in StringLengthComparisonOperator

--- tests/unit/test_check_operators/test_sql_string_comparison.py
+++ tests/unit/test_check_operators/test_sql_string_comparison.py
@@ -76,16 +76,16 @@ LONGER_THAN_TEST_DATA = [
         [True, False, True],
     ),
     (
-        {"target": ["", "A", "AB", "ABC"]},
+        {"target": ["C", "A", "AB", "ABC"]},
         0,
         True,
-        [False, True, True, True],
+        [True, True, True, True],
     ),
     (
-        {"target": ["A", "", "ABC"], "VAR2": ["", "XX", ""]},
+        {"target": ["A", "ABC"], "VAR2": ["", "XX"]},
         "VAR2",
         False,
-        [True, False, True],
+        [True, True],
     ),
     (
         {"target": ["A", "AB", "ABC"]},
@@ -94,10 +94,10 @@ LONGER_THAN_TEST_DATA = [
         [False, True, True],
     ),
     (
-        {"target": ["", "A", "AB", "ABC"]},
+        {"target": ["A", "AB", "ABC"]},
         "$constant",
         False,
-        [False, False, True, True],
+        [False, True, True],
     ),
 ]
 
@@ -125,6 +125,205 @@ def test_sql_longer_than(data, comparator, value_is_literal, expected_result):
 def test_sql_shorter_than_or_equal_to(data, comparator, value_is_literal, expected_result):
     sql_ops = create_sql_operators(data)
     result = sql_ops.shorter_than_or_equal_to(
+        {
+            "target": "target",
+            "comparator": comparator,
+            "value_is_literal": value_is_literal,
+        }
+    )
+    assert_series_equals(result, ~pd.Series(expected_result))
+
+
+LONGER_THAN_OR_EQUAL_TO_TEST_DATA = [
+    (
+        {"target": ["Att", "Btt", "Ctt"], "VAR2": ["A", "Bd", "ll"]},
+        "VAR2",
+        False,
+        [True, True, True],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctt"], "VAR2": ["ABC", "XYZ", "123"]},
+        "VAR2",
+        False,
+        [True, True, True],
+    ),
+    (
+        {"target": ["A", "BB", "CCC"], "VAR2": ["ABCD", "EFGH", "IJKL"]},
+        "VAR2",
+        False,
+        [False, False, False],
+    ),
+    (
+        {"target": ["A", "AB", "ABC", "ABCD"]},
+        "AB",
+        True,
+        [False, True, True, True],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctta"]},
+        3,
+        True,
+        [True, True, True],
+    ),
+    (
+        {"target": ["At", "Btt", "C"]},
+        2,
+        True,
+        [True, True, False],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctt"], "VAR2": [3, 2, 4]},
+        "VAR2",
+        False,
+        [True, True, False],
+    ),
+    (
+        {"target": ["A", "AB", "ABC"]},
+        0,
+        True,
+        [True, True, True],
+    ),
+    (
+        {"target": ["A", "AB"], "VAR2": ["", "XY"]},
+        "VAR2",
+        False,
+        [True, True],
+    ),
+    (
+        {"target": ["A", "AB", "ABC"]},
+        "$constant",
+        False,
+        [True, True, True],
+    ),
+    (
+        {"target": ["ABC", "AB", "A"]},
+        "$number",
+        False,
+        [True, True, True],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    LONGER_THAN_OR_EQUAL_TO_TEST_DATA,
+)
+def test_sql_longer_than_or_equal_to(data, comparator, value_is_literal, expected_result):
+    sql_ops = create_sql_operators(data)
+    result = sql_ops.longer_than_or_equal_to(
+        {
+            "target": "target",
+            "comparator": comparator,
+            "value_is_literal": value_is_literal,
+        }
+    )
+    assert_series_equals(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    LONGER_THAN_OR_EQUAL_TO_TEST_DATA,
+)
+def test_sql_shorter_than(data, comparator, value_is_literal, expected_result):
+    sql_ops = create_sql_operators(data)
+    result = sql_ops.shorter_than(
+        {
+            "target": "target",
+            "comparator": comparator,
+            "value_is_literal": value_is_literal,
+        }
+    )
+
+    assert_series_equals(result, ~pd.Series(expected_result))
+
+
+HAS_EQUAL_LENGTH_TEST_DATA = [
+    (
+        {"target": ["Att", "Btt", "Ctt"], "VAR2": ["Add", "Bee", "Cat"]},
+        "VAR2",
+        False,
+        [True, True, True],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctta"], "VAR2": ["A", "Bd", "lll"]},
+        "VAR2",
+        False,
+        [False, False, False],
+    ),
+    (
+        {"target": ["A", "AB", "ABC", "ABCD"]},
+        "AB",
+        True,
+        [False, True, False, False],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctta"]},
+        3,
+        True,
+        [True, True, False],
+    ),
+    (
+        {"target": ["At", "Btt", "Ct"]},
+        2,
+        True,
+        [True, False, True],
+    ),
+    (
+        {"target": ["Att", "Btt", "Ctt"], "VAR2": [3, 3, 3]},
+        "VAR2",
+        False,
+        [True, True, True],
+    ),
+    (
+        {"target": ["C", "A", "AB"]},
+        0,
+        True,
+        [False, False, False],
+    ),
+    (
+        {"target": ["A", "AB"], "VAR2": ["X", "YZ"]},
+        "VAR2",
+        False,
+        [True, True],
+    ),
+    (
+        {"target": ["A", "AB", "ABC"]},
+        "$constant",
+        False,
+        [True, False, False],
+    ),
+    (
+        {"target": ["ABC", "AB", "A"]},
+        "$number",
+        False,
+        [False, False, True],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    HAS_EQUAL_LENGTH_TEST_DATA,
+)
+def test_sql_has_equal_length(data, comparator, value_is_literal, expected_result):
+    sql_ops = create_sql_operators(data)
+    result = sql_ops.has_equal_length(
+        {
+            "target": "target",
+            "comparator": comparator,
+            "value_is_literal": value_is_literal,
+        }
+    )
+    assert_series_equals(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "data,comparator,value_is_literal,expected_result",
+    HAS_EQUAL_LENGTH_TEST_DATA,
+)
+def test_sql_has_not_equal_length(data, comparator, value_is_literal, expected_result):
+    sql_ops = create_sql_operators(data)
+    result = sql_ops.has_not_equal_length(
         {
             "target": "target",
             "comparator": comparator,
