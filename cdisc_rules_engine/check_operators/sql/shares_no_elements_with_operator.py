5ad7205d9cc2d8e67422f44bec3d1e86742b97e9
Samuel Johnson <sfjohnson24@gmail.com>
2025-09-23 15:58:35 -0400

    sql query line length reduction

--- cdisc_rules_engine/check_operators/sql/shares_no_elements_with_operator.py
+++ cdisc_rules_engine/check_operators/sql/shares_no_elements_with_operator.py
@@ -5,13 +5,25 @@ class SharesNoElementsWithOperator(BaseSqlOperator):
     """Operator for checking if values share no elements."""
 
     def execute_operator(self, other_value):
-        """target: str = self.replace_prefix(other_value.get("target"))
-        comparator: str = self.replace_prefix(other_value.get("comparator"))
+        target = self.replace_prefix(other_value.get("target"))
+        target_column = self._column_sql(target)
+        comparator = other_value.get("comparator")
+        value_is_literal = other_value.get("value_is_literal", False)
+        if not value_is_literal:
+            comparator = self.replace_prefix(comparator)
+        comparator_column = (
+            self._column_sql(comparator) if not value_is_literal else self._sql(comparator, value_is_literal=True)
+        )
+        operator_name = f"{target}_shares_no_elements_with_{comparator}"
 
-        def check_no_shared_elements(row):
-            target_set = set(row[target]) if isinstance(row[target], (list, set)) else {row[target]}
-            comparator_set = set(row[comparator]) if isinstance(row[comparator], (list, set)) else {row[comparator]}
-            return len(target_set.intersection(comparator_set)) == 0
+        def sql():
+            target_array = f"string_to_array({target_column}::text, ',')"
+            comparator_array = f"string_to_array({comparator_column}::text, ',')"
+            return f"""CASE
+                    WHEN {self._is_empty_sql(target)} THEN TRUE
+                    WHEN {comparator_column} IS NULL THEN TRUE
+                    WHEN NOT ({target_array} && {comparator_array}) THEN TRUE
+                    ELSE FALSE
+                    END"""
 
-        return self.validation_df.apply(check_no_shared_elements, axis=1).all()"""
-        raise NotImplementedError("shares_no_elements_with check_operator not implemented")
+        return self._do_check_operator(operator_name, sql)
