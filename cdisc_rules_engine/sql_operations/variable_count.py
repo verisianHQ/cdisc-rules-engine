de1dc02fff5bda09724be95b7873d2c0de69b6ed
YetiGonk <maximo@verisian.com>
2025-10-30 16:43:54 +0000

    fixed variable count + regressions

--- cdisc_rules_engine/sql_operations/variable_count.py
+++ cdisc_rules_engine/sql_operations/variable_count.py
@@ -5,14 +5,25 @@ from cdisc_rules_engine.sql_operations.sql_base_operation import SqlBaseOperatio
 class SqlVariableCountOperation(SqlBaseOperation):
     def _execute_operation(self):
         all_tables = self.data_service.pgi.schema.get_tables()
-
         data_tables = [(name, schema) for name, schema in all_tables if schema.source == "data"]
 
+        target = self.params.target
+        domain = self.params.domain.upper()
+
+        if target.upper().startswith(domain):
+            wildcard_target = "--" + target[len(domain) :]
+        else:
+            wildcard_target = target
+
         count = 0
-        for _, table_schema in data_tables:
-            if table_schema.has_column(self.params.target):
+        for table_name, table_schema in data_tables:
+            if wildcard_target.startswith("--"):
+                target_variable = table_name.upper() + wildcard_target[2:]
+            else:
+                target_variable = wildcard_target
+
+            if table_schema.has_column(target_variable):
                 count += 1
 
         query = f"SELECT {count} AS value"
-
         return SqlOperationResult(query=query, type="constant", subtype="Num")
