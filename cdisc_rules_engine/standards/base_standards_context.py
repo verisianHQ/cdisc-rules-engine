d264f7bb12adccce2ac7359c091ee3acfcae1e2d
rich-verisian <richard@verisian.com>
2025-11-12 17:02:43 +0000

    added valid_codelist_dates op

--- cdisc_rules_engine/standards/base_standards_context.py
+++ cdisc_rules_engine/standards/base_standards_context.py
@@ -27,6 +27,10 @@ class BaseStandardsContext(ABC):
         pass
 
     @abstractmethod
+    def get_ct_packages(self):
+        pass
+
+    @abstractmethod
     def within_rule_scope(self, scope: dict[str, Any], metadata: DatasetMetadata2):
         pass
 
