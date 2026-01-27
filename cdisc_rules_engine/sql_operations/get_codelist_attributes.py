7d0ea4a1dc413175cdcd6c9fe29e815b50722bf0
Maximo <maximo@verisian.com>
2026-01-27 14:40:53 +0000

    better formatting

--- cdisc_rules_engine/sql_operations/get_codelist_attributes.py
+++ cdisc_rules_engine/sql_operations/get_codelist_attributes.py
@@ -21,7 +21,6 @@ class SqlGetCodelistAttributesOperation(SqlBaseOperation):
         ct_table = StaticTables.IG_CODELIST_TABLE_NAME.value
         attribute = self.params.ct_attribute
         version = self.params.ct_version
-        domain = self.params.domain
 
         select_col_sql = self.data_service.pgi.schema.get_column_hash(ct_table, _column_map.get(attribute, "item_code"))
         version_date_col_sql = self.data_service.pgi.schema.get_column_hash(ct_table, "version_date")
@@ -31,20 +30,11 @@ class SqlGetCodelistAttributesOperation(SqlBaseOperation):
         ct_types = {ct.split("ct")[0] for ct in ct_packages}
         types_list_sql = ", ".join([f"'{t}'" for t in ct_types])
 
-        op_params = {}
-        if self.data_service.pgi.schema.column_exists(domain, version):
-            # version is a column name
-            version_val_sql = "$1"
-            op_params[version_val_sql] = version.lower()
-        else:
-            # version is a date string literal
-            version_val_sql = f"'{version}'"
-
         query = f"""
             SELECT DISTINCT {select_col_sql} AS value
             FROM {ct_table}
             WHERE {std_type_col_sql} IN ({types_list_sql})
-              AND {version_date_col_sql} = {version_val_sql}
+              AND {version_date_col_sql} = $1
         """
 
-        return SqlOperationResult(query=query, type="collection", subtype="Char", params=op_params)
+        return SqlOperationResult(query=query, type="collection", subtype="Char", params={"$1": version.lower()})
