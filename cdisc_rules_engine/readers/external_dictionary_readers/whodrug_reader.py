e86711353d5c9499392ab5a6a9dcb33002d3885d
Maximo <maximo@verisian.com>
2026-03-16 17:34:44 +0000

    drug name added as column

--- cdisc_rules_engine/readers/external_dictionary_readers/whodrug_reader.py
+++ cdisc_rules_engine/readers/external_dictionary_readers/whodrug_reader.py
@@ -49,21 +49,55 @@ class WhoDrugReader:
         else:
             raise ValueError(f"Version information in {file_path} does not match expected format.")
 
+    def _apply_file_mapping(
+        self,
+        input_df: pd.DataFrame,
+        file_name: str,
+        col_map: dict[int, str],
+        join_key: str,
+        target_col: str,
+    ) -> pd.DataFrame:
+
+        file_path = f"{self.dictionary_path}/{file_name}.csv"
+
+        if os.path.exists(file_path):
+            indices = list(col_map.keys())
+            col_names = list(col_map.values())
+
+            df = pd.read_csv(
+                file_path,
+                header=None,
+                dtype=str,
+                usecols=indices,
+                names=col_names,
+            )
+
+            df[join_key] = df[join_key].str.strip()
+
+            return pd.merge(input_df, df, on=join_key, how="left")
+
+        final_df = input_df.copy()
+        final_df[target_col] = None
+        return final_df
+
     def process_data(self, metadata: WhoDrugVersionMetadata) -> pd.DataFrame:
         """
         Reads the active ingredient data for C3 and B3 formats.
         Processes the ATC hierarchy levels into flattened level 4 codes.
-        Merges them to include the format's primary identifier.
+        Merges them to include the format's primary identifier and drug name.
         """
         atc_file_name = (
             C3WHODrugFiles.ATC.value if metadata.format == WhoDrugFormats.C3.value else B3WHODrugFiles.INA.value
         )
         atc_file_path = f"{self.dictionary_path}/{atc_file_name}.csv"
+
         atc_df = pd.read_csv(atc_file_path, header=None, dtype=str, names=["atc_code", "level", "atc_text"])
+
         atc_df["atc_code"] = atc_df["atc_code"].str.strip()
         atc_df["atc_text"] = atc_df["atc_text"].str.strip()
         code_to_text_map = dict(zip(atc_df["atc_code"], atc_df["atc_text"]))
         level_4_codes = atc_df[atc_df["level"] == "4"]
+
         result_df = pd.DataFrame({"atc_code": level_4_codes["atc_code"]})
         level_to_length = {
             "level_1": 1,
@@ -78,55 +112,38 @@ class WhoDrugReader:
             )
 
         if metadata.format == WhoDrugFormats.C3.value:
-            final_df = self._get_med_prod_id_mapping(result_df)
+            med_prod_df = self._apply_file_mapping(
+                input_df=result_df,
+                file_name=C3WHODrugFiles.ThG.value,
+                col_map={1: "atc_code", 4: "med_prod_id"},
+                join_key="atc_code",
+                target_col="med_prod_id",
+            )
+            final_df = self._apply_file_mapping(
+                input_df=med_prod_df,
+                file_name=C3WHODrugFiles.MP.value,
+                col_map={0: "med_prod_id", 8: "drug_name"},
+                join_key="med_prod_id",
+                target_col="drug_name",
+            )
+
         elif metadata.format == WhoDrugFormats.B3.value:
-            final_df = self._get_drug_rec_num_mapping(result_df)
-
-        return final_df
-
-    def _get_med_prod_id_mapping(self, atc_df: pd.DataFrame) -> pd.DataFrame:
-        thg_file_path = f"{self.dictionary_path}/{C3WHODrugFiles.ThG.value}.csv"
-        if os.path.exists(thg_file_path):
-            thg_df = pd.read_csv(
-                thg_file_path,
-                header=None,
-                dtype=str,
-                names=["thg_id", "atc_code", "create_date", "official_atc_code", "med_prod_id"],
+            drug_rec_df = self._apply_file_mapping(
+                input_df=result_df,
+                file_name=B3WHODrugFiles.DDA.value,
+                col_map={0: "drug_rec_num", 4: "atc_code"},
+                join_key="atc_code",
+                target_col="drug_rec_num",
+            )
+            final_df = self._apply_file_mapping(
+                input_df=drug_rec_df,
+                file_name=B3WHODrugFiles.DD.value,
+                col_map={0: "drug_rec_num", 11: "drug_name"},
+                join_key="drug_rec_num",
+                target_col="drug_name",
             )
 
-            thg_subset = thg_df[["atc_code", "med_prod_id"]].copy()
-            thg_subset["atc_code"] = thg_subset["atc_code"].str.strip()
-
-            final_df = pd.merge(atc_df, thg_subset, on="atc_code", how="left")
         else:
-            final_df = atc_df.copy()
-            final_df["med_prod_id"] = None
-
-        return final_df
-
-    def _get_drug_rec_num_mapping(self, atc_df: pd.DataFrame) -> pd.DataFrame:
-        dda_file_path = f"{self.dictionary_path}/{B3WHODrugFiles.DDA.value}.csv"
-        if os.path.exists(dda_file_path):
-            dda_df = pd.read_csv(
-                dda_file_path,
-                header=None,
-                dtype=str,
-                names=[
-                    "drug_rec_num",
-                    "seq_num_1",
-                    "seq_num_2",
-                    "check_digit",
-                    "atc_code",
-                    "year_quarter",
-                    "official_atc_code",
-                ],
-            )
-            dda_subset = dda_df[["drug_rec_num", "atc_code"]].copy()
-            dda_subset["atc_code"] = dda_subset["atc_code"].str.strip()
-
-            final_df = pd.merge(atc_df, dda_subset, on="atc_code", how="left")
-        else:
-            final_df = atc_df.copy()
-            final_df["drug_rec_num"] = None
+            raise ValueError(f"Unsupported format: {metadata.format}")
 
         return final_df
