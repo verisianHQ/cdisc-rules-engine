6a874a3bf15a46f87463b3d28241212b45fc265a
rich-verisian <richard@verisian.com>
2026-04-07 16:37:54 +0100

    remove comments

--- cdisc_rules_engine/standards/sdtm_standards_context.py
+++ cdisc_rules_engine/standards/sdtm_standards_context.py
@@ -303,7 +303,6 @@ class SdtmStandardsContext(BaseStandardsContext):
             reason = f"Rule skipped - doesn't apply to class for " f"rule id={rule_id}, dataset={dataset_name}"
             logger.info(f"is_suitable_for_validation. {reason}, result=False")
             return False, reason
-        # TODO: Need to fix is_split
         if not self.rule_applies_to_domain(metadata, rule, domain, is_split=is_split):
             reason = f"Rule skipped - doesn't apply to domain for rule id={rule_id}, dataset={dataset_name}"
             logger.info(f"is_suitable_for_validation. {reason}, result=False")
