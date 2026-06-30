57536f2505973e7e07984def78620259d74eae53
rich-verisian <richard@verisian.com>
2026-06-30 03:34:07 +0100

    fix tests

--- tests/unit/cdisc/test_services/test_data_service/test_data_service.py
+++ tests/unit/cdisc/test_services/test_data_service/test_data_service.py
@@ -189,6 +189,7 @@ def test_get_dataset_class(dataset_metadata, data, expected_class):
             None,
             "",
             None,
+            None,
             False,
             "",
         )
