66200376ad5aef0e845592d9c836076377b09293
rich-verisian <richard@verisian.com>
2026-07-29 13:33:31 +0100

    adam scope functionality

--- tests/unit/sql/test_services/test_data_service/test_postgresql_data_service.py
+++ tests/unit/sql/test_services/test_data_service/test_postgresql_data_service.py
@@ -1,6 +1,10 @@
+from cdisc_rules_engine.constants.data_structures import BDS
 from cdisc_rules_engine.data_service.postgresql_data_service import (
     PostgresQLDataService,
 )
+from cdisc_rules_engine.models.library_metadata_container import LibraryMetadataContainer
+from cdisc_rules_engine.models.test_dataset import TestDataset as DatasetFixture
+from cdisc_rules_engine.standards.adam_standards_context import AdamStandardsContext
 from cdisc_rules_engine.standards.default_standards_context import (
     DefaultStandardsContext,
 )
@@ -30,3 +34,20 @@ def test_get_uploaded_dataset_ids(get_sample_lb_dataset, get_sample_supp_dataset
         [get_sample_lb_dataset, get_sample_supp_dataset], DefaultStandardsContext()
     )
     assert 2 == len(sql_data_service.get_uploaded_dataset_ids())
+
+
+def test_adam_dataset_class_assigned_during_sql_load():
+    dataset = DatasetFixture.from_records(
+        "ADVS",
+        {
+            "STUDYID": ["STUDY1"],
+            "USUBJID": ["STUDY1-001"],
+            "PARAMCD": ["SYSBP"],
+            "AVAL": [120],
+        },
+    )
+    context = AdamStandardsContext(LibraryMetadataContainer())
+
+    sql_data_service = PostgresQLDataService.from_list_of_testdatasets([dataset], context)
+
+    assert sql_data_service.get_dataset_metadata("ADVS").dataset_class == BDS
