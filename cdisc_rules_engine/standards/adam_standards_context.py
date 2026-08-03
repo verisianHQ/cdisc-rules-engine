2aee18baa0abf47656a1b3c676804f60ae293f5d
enrymather-Verisian <enrico@verisian.com>
2026-08-03 19:05:42 +0200

    added MD indicators set

--- cdisc_rules_engine/standards/adam_standards_context.py
+++ cdisc_rules_engine/standards/adam_standards_context.py
@@ -6,10 +6,9 @@ from cdisc_rules_engine.constants.data_structures import (
     ADAM_DATA_STRUCTURE_ALIASES,
     BDS,
     BDS_INDICATORS,
+    MD_INDICATORS,
     MDBDS,
-    MDBDS_INDICATORS,
     MDOCCDS,
-    MDOCCDS_SUFFIXES,
     OCCDS,
     OCCDS_SUFFIXES,
     OTHER,
@@ -41,13 +40,15 @@ class AdamStandardsContext(DefaultStandardsContext):
 
         columns = {variable.name.upper() for variable in dataset_metadata.variables}
         if columns.intersection(BDS_INDICATORS):
-            return BDS
-        if columns.intersection(MDBDS_INDICATORS):
-            return MDBDS
+            if columns.intersection(MD_INDICATORS):
+                return MDBDS
+            else:
+                return BDS
         if any(column.endswith(tuple(OCCDS_SUFFIXES)) for column in columns):
-            return OCCDS
-        if any(column.endswith(tuple(MDOCCDS_SUFFIXES)) for column in columns):
-            return MDOCCDS
+            if columns.intersection(MD_INDICATORS):
+                return MDOCCDS
+            else:
+                return OCCDS
         return OTHER
 
     def derive_domain(self, filename: str):
